7d1acf4155b95fd431a8c66842087dc44977a40c
Mateus Schverz <matefs8569@gmail.com>
2023-12-18 22:51:03 -0300

    refactor: adding whitespaces

--- aula_ninja/urls.py
+++ aula_ninja/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('admin/',admin.site.urls),
-    path("cadastro/",include('cadastro.urls'))
+    path('admin/', admin.site.urls),
+    path("cadastro/", include('cadastro.urls'))
 ]
