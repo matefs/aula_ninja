1c8b0494b76f8eacbc1d80e468326b9fdae4212b
Mateus Schverz <matefs8569@gmail.com>
2023-12-31 21:07:50 -0300

    fix(view): fix wrong import sintax

--- aula_ninja/urls.py
+++ aula_ninja/urls.py
@@ -1,12 +1,12 @@
 # https://www.youtube.com/watch?v=jMDWdh1zhf4&ab_channel=pythonando
 from django.contrib import admin
 from django.urls import path, include
-from ..cadastro.views import minha_view
+#from ..cadastro.views import minha_view
+from cadastro.views import minha_view
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("cadastro/", include('cadastro.urls')),
-    # path('', minha_view, name='minha-view'),
-
+    path('', minha_view, name='minha-view'),
 ]
