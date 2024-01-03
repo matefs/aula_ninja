ae20119015b066bdbe16763f4fcf4f7ea4e22472
Mateus Schverz <mate_fs@hotmail.com>
2024-01-02 23:12:51 -0300

    💥breaking feat(command): trying to add a new django command using "Livros" model

---
+++ management/commands/getallbooks.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand
+from cadastro.models import Livro
+
+class Command(BaseCommand):
+    help = "Get all books from the database"
+
+    def handle(self, *args, **options):
+        books = Livro.objects.all()
+
+        if books:
+            self.stdout.write("List of all books:")
+            for book in books:
+                self.stdout.write(f"- {book}")
+        else:
+            self.stdout.write("No books found in the database.")
