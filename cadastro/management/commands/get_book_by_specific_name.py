03e5667ed3b4e2e04734ac457522ed2406ab1714
Mateus Schverz <mate_fs@hotmail.com>
2024-01-06 23:04:52 -0300

    Create get_book_by_specific_name.py

---
+++ cadastro/management/commands/get_book_by_specific_name.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand
+from cadastro.models import Livro
+
+class Command(BaseCommand):
+    help = "Get all books from the database"
+
+    def handle(self, *args, **options):
+        books = Livro.objects.filter(id__gt=1)
+
+        if books:
+            self.stdout.write("List of all books:")
+            for book in books:
+                self.stdout.write(f"- {book}")
+        else:
+            self.stdout.write("No books found in the database.")
+
+
