c9dd83203db9a2dfd48520e240649cf5cd336623
Mateus Schverz <matefs8569@gmail.com>
2024-02-13 18:19:35 -0300

    add: ninja api swagger

--- aula_ninja/urls.py
+++ aula_ninja/urls.py
@@ -3,10 +3,14 @@ from django.contrib import admin
 from django.urls import path, include
 #from ..cadastro.views import minha_view
 from cadastro.views import minha_view
+from ninja import NinjaAPI
+
+api = NinjaAPI()
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("cadastro/", include('cadastro.urls')),
+    path('api/', api.urls),  # Esta linha adiciona as URLs do Django Ninja
     path('', minha_view, name='minha-view'),
 ]
--- cadastro/views.py
+++ cadastro/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 def minha_view(request):
     # Lógica da sua visualização
-    resposta_texto = "Bem-vindo à minha página!"
+    resposta_texto = "Tente acessar /api/docs"
     return HttpResponse(resposta_texto, content_type="text/plain")
